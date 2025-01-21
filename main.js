2045e54424c9d4e1d4d3bfe7fe5e3dbd329c0d17
saran612 <freelancexsaran@gmail.com>
2025-01-21 17:57:57 +0530

    js add

---
+++ main.js
@@ -0,0 +1,22 @@
+document.addEventListener('contextmenu', function(event) {
+    event.preventDefault();
+});
+
+document.addEventListener('keydown', function(event) {
+    // Disable F12
+    if (event.key === 'F12') {
+        event.preventDefault();
+    }
+    // Disable Ctrl+Shift+I
+    if (event.ctrlKey && event.shiftKey && event.key === 'I') {
+        event.preventDefault();
+    }
+    // Disable Ctrl+Shift+J
+    if (event.ctrlKey && event.shiftKey && event.key === 'J') {
+        event.preventDefault();
+    }
+    // Disable Ctrl+U
+    if (event.ctrlKey && event.key === 'U') {
+        event.preventDefault();
+    }
+});
