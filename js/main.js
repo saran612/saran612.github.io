c3ef4f3533867362f6b4466dc99526888dbf1f61
saran612 <freelancexsaran@gmail.com>
2025-01-30 08:45:08 +0530

    js update

--- js/main.js
+++ js/main.js
@@ -1,21 +1,4 @@
-document.addEventListener('contextmenu', function(event) {
-    event.preventDefault();
-});
 
-document.addEventListener('keydown', function(event) {
-    if (event.key === 'F12') {
-        event.preventDefault();
-    }
-    if (event.ctrlKey && event.shiftKey && event.key === 'I') {
-        event.preventDefault();
-    }
-    if (event.ctrlKey && event.shiftKey && event.key === 'J') {
-        event.preventDefault();
-    }
-    if (event.ctrlKey && event.key === 'U') {
-        event.preventDefault();
-    }
-});
 window.onload = function() {
     setTimeout(function() {
       document.getElementById('loader').classList.add('dis');
