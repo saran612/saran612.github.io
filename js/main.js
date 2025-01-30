7f96cb4bb9870e769f908f3734f50039277eb96a
saran612 <freelancexsaran@gmail.com>
2025-01-30 11:04:38 +0530

    don

--- js/main.js
+++ js/main.js
@@ -3,11 +3,10 @@ window.onload = function() {
     setTimeout(function() {
       document.getElementById('loader').classList.add('dis');
       document.getElementById('bg').classList.add('reveal');
-    }, 1300);
+    }, 1100);
   };
 
-  const fonts = ["Roboto","Redressed","Montserrat",
-     ,"Oswald","anta",
+  const fonts = [,"Redressed","Montserrat","anta",
      "prompt","acme","after","Alston","Aclonica","Greathy",
      "Mushroom","Euphoria"];
   let index = 0;
