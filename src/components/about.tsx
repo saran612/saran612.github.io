a0cfbff0d3c3cdb5c42fb69623d42d9ace843fe5
saran612 <freelancexsaran@gmail.com>
2025-10-08 09:57:51 +0530

    commit

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -26,9 +26,9 @@ export default function About() {
           <div className="md:col-span-2 flex flex-col">
             <Card className="flex-grow flex flex-col">
               <CardContent className="pt-6 flex-grow flex flex-col justify-center">
-                <h3 className="text-2xl md:text-3xl font-bold mb-2">Hi, I’m Saran Karthick</h3>
-                <p className="text-lg md:text-xl text-primary mb-4 font-semibold">I create intelligent web experiences.</p>
-                <p className="text-base md:text-lg text-foreground/80 leading-relaxed font-light">
+                <h2 className="text-4xl md:text-5xl font-bold mb-2">Hi, I’m Saran Karthick</h2>
+                <p className="text-lg md:text-2xl text-primary mb-4 font-semibold">I create intelligent web experiences.</p>
+                <p className="text-base md:text-xl text-foreground/80 leading-relaxed ">
                   I am a passionate Computer Science and Business Systems undergraduate at Bannari Amman Institute of Technology, and my passion lies at the intersection of web development, artificial intelligence, and automation.
                   <br />
                   <br />
